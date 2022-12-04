0ad4d6b31aefd899b113c21e35035bf87dfe9f53
Kalyan Mondal <kalyan.x.mondal@gmail.com>
2022-12-04 10:59:46 -0500

    code to upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const circle = document.getElementById("circle");
+const eachMove = 10;
+
+window.addEventListener('load', function(){
+    circle.style.position = 'absolute'
+    circle.style.left = 0; 
+    circle.style.top = 0;
+});
+
+window.addEventListener('keyup', function(e){
+ switch(e.key){
+    case 'ArrowLeft':
+        circle.style.left = parseInt(circle.style.left)-eachMove+'px';
+        break;
+    case 'ArrowRight':
+        circle.style.left = parseInt(circle.style.left)+eachMove+'px';
+        break;
+    case 'ArrowDown':
+        circle.style.top = parseInt(circle.style.top)+eachMove+'px';
+        break;
+    case 'ArrowUp':
+        circle.style.top = parseInt(circle.style.top)-eachMove+'px';
+        break;
+
+ }
+});
