19b1c3a26c0ba426a22967e66e35fe11ea95f537
Kalyan Mondal <kalyan.x.mondal@gmail.com>
2022-12-05 16:27:11 -0500

    t24bbbg23btrw eh2g4rwfr

--- script.js
+++ script.js
@@ -16,20 +16,29 @@ window.addEventListener('keydown', function(e){
  switch(e.key){
     case 'ArrowLeft':
         circle.style.left = parseInt(circle.style.left)-eachMove+'px';
+        console.log(circle.style.left)
         break;
     case 'ArrowRight':
         circle.style.left = parseInt(circle.style.left)+eachMove+'px';
+        console.log(circle.style.left)
         break;
     case 'ArrowDown':
         circle.style.top = parseInt(circle.style.top)+eachMove+'px';
+        console.log(circle.style.left)
         break;
     case 'ArrowUp':
         circle.style.top = parseInt(circle.style.top)-eachMove+'px';
+        console.log(circle.style.left)
         break;
-    
-    
+
  }
 });
 
+window.addEventListener('click', function(){
+    const newThing = document.createElement('div')
+    newThing.style.height = 100 +'px';
+    newThing.style.width = 100 +'px';
+    newThing.style.borderRadius = 50+"%";
+    newThing.style.backgroundColor = 'black';
 
-
+})
